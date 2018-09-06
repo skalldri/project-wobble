67883630503a1e25904c24850a5f500e53853467
kvaillancourt <vaillancourt.kara@gmail.com>
2018-09-05 22:08:45 -0700

    readme changes

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 ########################## project-wobble ##########################
 This is a program designed to incorporate several hardware components to produce a self balancing robot.
-Authors: Stuart Alldrit <skalldrit>
+Authors: Stuart Alldrit <skalldri>
          Kara Vaillancourt <kvaillancourt>
 
 #################### HARDWARE ####################
-Nordic nrf52
-MPU-9250
+Nordic nrf52 - Microcontroller, bluetooth radio 
+    http://infocenter.nordicsemi.com/pdf/nRF52_DK_User_Guide_v1.2.pdf
+    http://infocenter.nordicsemi.com/pdf/nRF52832_PS_v1.1.pdf
+MPU-9250 - Inertial Measurement Unit
+    https://www.invensense.com/wp-content/uploads/2015/02/PS-MPU-9250A-01-v1.1.pdf
+DRV8825 - Stepper Motor Driver
+    http://www.ti.com/lit/ds/symlink/drv8825.pdf
+DIYmall OLED - screen
 
 #################### DEPENDANCIES ####################
 Chocolatey (Windows Only) - skip this section and run env.ps1 instead
@@ -18,14 +24,32 @@ python - dependancy for ninja
 ZEPHYR - install all dependancies. Follow this guide:
 http://docs.zephyrproject.org/getting_started/getting_started.html
 
+Ninja menu config:
+go into build folder:
+> ninja menuconfig
+Device Drivers > Console Drivers > Use RTT Console * (output all the debug statements)
+Device Drivers > SPI Hardware Bus Drivers > SPI port 2 * (port to talk to the mpu9250)
+Device Drivers > SPI Hardware Bus Drivers > Port 2 interrupt priority (2)
+Device Drivers > SPI Hardware Bus Drivers > nRF SPI nrfx drivers > SPI Port 2 Driver type > SCK pin number (15)
+Device Drivers > SPI Hardware Bus Drivers > nRF SPI nrfx drivers > SPI Port 2 Driver type > MOSI pin number (14)
+Device Drivers > SPI Hardware Bus Drivers > nRF SPI nrfx drivers > SPI Port 2 Driver type > MISO pin number (13) 
+Device Drivers > I2C Drivers > Enable I2C Port 0
+Device Drivers > I2C Drivers > Enable I2C Port 1
+Device Drivers > I2C Drivers > nrf TWI nrfx drivers > I2C Port 0 Driver type (nerf TWI 0) > nrf TWI 0 (x)
+Device Drivers > I2C Drivers > nrf TWI nrfx drivers > I2C Port 1 Driver type (nerf TWI 1) > nrf TWI 1 (x)
+Device Drivers > Sensor Drivers > nrf5 Temperature Sensor > (TEMP 0) Driver Name
+Device Drivers > Sensor Drivers > nrf5 Temperature Sensor > (1) TEMP interrupt priority
+
+> o (select 'prj.conf' file inside project wobble folder) (loads all files in the file)
+
 #################### SETUP ####################
 Set Enviorment Variables: 
 You must do this EVERY new terminal session. The command is:
     export <VAR_NAME> = <path/to/variable>
 
-EPHYR_BASE=/Users/kvaillancourt/CLionProjects/project-wobble/zephyr
+EPHYR_BASE=<project/folder>/project-wobble/zephyr
 ZEPHYR_TOOLCHAIN_VARIANT=gccarmemb
-GCCARMEMB_TOOLCHAIN_PATH=/Users/kvaillancourt/gcc-arm-none-eabi-7-2018-q2-update
+GCCARMEMB_TOOLCHAIN_PATH=<gcc/folder>/gcc-arm-none-eabi-7-2018-q2-update
 
 Run Project:
 Cmake -GNinja -DBOARD=nrf52_pca10040 ../app/ 
@@ -33,6 +57,14 @@ Cmake -GNinja -DBOARD=nrf52_pca10040 ../app/
 Detect board for Debugging: 
 JLinkGDBServer -if SWD -device NRF52832_XXAA
 
-Debug:
-Open telnet
-telnet> open localhost 19021
+#################### DEBUG ####################
+
+OS Print:
+    Open telnet
+    telnet> open localhost 19021
+
+Download & Run Firmware:
+    Open VSCode
+    Debug symbol
+    Play symbol
+    (Debug breaks will ensue)
--- drivers/sensor/mpu9250/mpu9250.c
+++ drivers/sensor/mpu9250/mpu9250.c
@@ -2,6 +2,9 @@
  * Copyright (c) 2016 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Convert the 5060 driver into a 9250 driver, make it work over spi instead of i2c
+ * imu.c should be in the app folder
  */
 
 #include <i2c.h>
@@ -9,10 +12,10 @@
 #include <misc/byteorder.h>
 #include <sensor.h>
 
-#include "mpu6050.h"
+#include "mpu9250.h"
 
 /* see "Accelerometer Measurements" section from register map description */
-static void mpu6050_convert_accel(struct sensor_value *val, s16_t raw_val,
+static void mpu9250_convert_accel(struct sensor_value *val, s16_t raw_val,
 				  u16_t sensitivity_shift)
 {
 	s64_t conv_val;
@@ -23,7 +26,7 @@ static void mpu6050_convert_accel(struct sensor_value *val, s16_t raw_val,
 }
 
 /* see "Gyroscope Measurements" section from register map description */
-static void mpu6050_convert_gyro(struct sensor_value *val, s16_t raw_val,
+static void mpu9250_convert_gyro(struct sensor_value *val, s16_t raw_val,
 				 u16_t sensitivity_x10)
 {
 	s64_t conv_val;
@@ -35,7 +38,7 @@ static void mpu6050_convert_gyro(struct sensor_value *val, s16_t raw_val,
 }
 
 /* see "Temperature Measurement" section from register map description */
-static inline void mpu6050_convert_temp(struct sensor_value *val,
+static inline void mpu9250_convert_temp(struct sensor_value *val,
 					s16_t raw_val)
 {
 	val->val1 = raw_val / 340 + 36;
@@ -50,67 +53,69 @@ static inline void mpu6050_convert_temp(struct sensor_value *val,
 	}
 }
 
-static int mpu6050_channel_get(struct device *dev,
+//pull
+static int mpu9250_channel_get(struct device *dev,
 			       enum sensor_channel chan,
 			       struct sensor_value *val)
 {
-	struct mpu6050_data *drv_data = dev->driver_data;
+	struct mpu9250_data *drv_data = dev->driver_data;
 
 	switch (chan) {
 	case SENSOR_CHAN_ACCEL_XYZ:
-		mpu6050_convert_accel(val, drv_data->accel_x,
+		mpu9250_convert_accel(val, drv_data->accel_x,
 				      drv_data->accel_sensitivity_shift);
-		mpu6050_convert_accel(val + 1, drv_data->accel_y,
+		mpu9250_convert_accel(val + 1, drv_data->accel_y,
 				      drv_data->accel_sensitivity_shift);
-		mpu6050_convert_accel(val + 2, drv_data->accel_z,
+		mpu9250_convert_accel(val + 2, drv_data->accel_z,
 				      drv_data->accel_sensitivity_shift);
 		break;
 	case SENSOR_CHAN_ACCEL_X:
-		mpu6050_convert_accel(val, drv_data->accel_x,
+		mpu9250_convert_accel(val, drv_data->accel_x,
 				      drv_data->accel_sensitivity_shift);
 		break;
 	case SENSOR_CHAN_ACCEL_Y:
-		mpu6050_convert_accel(val, drv_data->accel_y,
+		mpu9250_convert_accel(val, drv_data->accel_y,
 				      drv_data->accel_sensitivity_shift);
 		break;
 	case SENSOR_CHAN_ACCEL_Z:
-		mpu6050_convert_accel(val, drv_data->accel_z,
+		mpu9250_convert_accel(val, drv_data->accel_z,
 				      drv_data->accel_sensitivity_shift);
 		break;
 	case SENSOR_CHAN_GYRO_XYZ:
-		mpu6050_convert_gyro(val, drv_data->gyro_x,
+		mpu9250_convert_gyro(val, drv_data->gyro_x,
 				     drv_data->gyro_sensitivity_x10);
-		mpu6050_convert_gyro(val + 1, drv_data->gyro_y,
+		mpu9250_convert_gyro(val + 1, drv_data->gyro_y,
 				     drv_data->gyro_sensitivity_x10);
-		mpu6050_convert_gyro(val + 2, drv_data->gyro_z,
+		mpu9250_convert_gyro(val + 2, drv_data->gyro_z,
 				     drv_data->gyro_sensitivity_x10);
 		break;
 	case SENSOR_CHAN_GYRO_X:
-		mpu6050_convert_gyro(val, drv_data->gyro_x,
+		mpu9250_convert_gyro(val, drv_data->gyro_x,
 				     drv_data->gyro_sensitivity_x10);
 		break;
 	case SENSOR_CHAN_GYRO_Y:
-		mpu6050_convert_gyro(val, drv_data->gyro_y,
+		mpu9250_convert_gyro(val, drv_data->gyro_y,
 				     drv_data->gyro_sensitivity_x10);
 		break;
 	case SENSOR_CHAN_GYRO_Z:
-		mpu6050_convert_gyro(val, drv_data->gyro_z,
+		mpu9250_convert_gyro(val, drv_data->gyro_z,
 				     drv_data->gyro_sensitivity_x10);
 		break;
 	default: /* chan == SENSOR_CHAN_DIE_TEMP */
-		mpu6050_convert_temp(val, drv_data->temp);
+		mpu9250_convert_temp(val, drv_data->temp);
 	}
 
 	return 0;
 }
 
-static int mpu6050_sample_fetch(struct device *dev, enum sensor_channel chan)
+//main updater
+static int mpu9250_sample_fetch(struct device *dev, enum sensor_channel chan)
 {
-	struct mpu6050_data *drv_data = dev->driver_data;
+	struct mpu9250_data *drv_data = dev->driver_data;
 	s16_t buf[7];
 
-	if (i2c_burst_read(drv_data->i2c, CONFIG_MPU6050_I2C_ADDR,
-			   MPU6050_REG_DATA_START, (u8_t *)buf, 14) < 0) {
+	if (i2c_burst_read(drv_data->i2c, CONFIG_MPU9250_I2C_ADDR,
+			   MPU9250_REG_DATA_START, (u8_t *)buf, 14) < 0) {
 		SYS_LOG_ERR("Failed to read data sample.");
 		return -EIO;
 	}
@@ -126,38 +131,39 @@ static int mpu6050_sample_fetch(struct device *dev, enum sensor_channel chan)
 	return 0;
 }
 
-static const struct sensor_driver_api mpu6050_driver_api = {
-#if CONFIG_MPU6050_TRIGGER
-	.trigger_set = mpu6050_trigger_set,
+//push
+static const struct sensor_driver_api mpu9250_driver_api = {
+#if CONFIG_MPU9250_TRIGGER
+	.trigger_set = mpu9250_trigger_set,
 #endif
-	.sample_fetch = mpu6050_sample_fetch,
-	.channel_get = mpu6050_channel_get,
+	.sample_fetch = mpu9250_sample_fetch,
+	.channel_get = mpu9250_channel_get,
 };
 
-int mpu6050_init(struct device *dev)
+int mpu9250_init(struct device *dev)
 {
-	struct mpu6050_data *drv_data = dev->driver_data;
+	struct mpu9250_data *drv_data = dev->driver_data;
 	u8_t id, i;
 
-	drv_data->i2c = device_get_binding(CONFIG_MPU6050_I2C_MASTER_DEV_NAME); // This is the name of the bus in ninja config menu - kconfig (SPI_1)
+	drv_data->i2c = device_get_binding(CONFIG_MPU9250_I2C_MASTER_DEV_NAME); // This is the name of the bus in ninja config menu - kconfig (SPI_1)
 	// we didn't find the bus to talk to the chip, fail
 	if (drv_data->i2c == NULL) {
 		SYS_LOG_ERR("Failed to get pointer to %s device",
-			    CONFIG_MPU6050_I2C_MASTER_DEV_NAME);
+			    CONFIG_MPU9250_I2C_MASTER_DEV_NAME);
 		return -EINVAL;
 	}
 
 	/* check chip ID
 		if the chip is connected to the bus physically
 	 */
-	if (i2c_reg_read_byte(drv_data->i2c, CONFIG_MPU6050_I2C_ADDR,
-			      MPU6050_REG_CHIP_ID, &id) < 0) {
+	if (i2c_reg_read_byte(drv_data->i2c, CONFIG_MPU9250_I2C_ADDR,
+			      MPU9250_REG_CHIP_ID, &id) < 0) {
 		SYS_LOG_ERR("Failed to read chip ID.");
 		return -EIO;
 	}
 
 	// bail if wrong chip id
-	if (id != MPU6050_CHIP_ID) {
+	if (id != MPU9250_CHIP_ID) {
 		SYS_LOG_ERR("Invalid chip ID.");
 		return -EINVAL;
 	}
@@ -165,8 +171,8 @@ int mpu6050_init(struct device *dev)
 	/* wake up chip
 	// reads and writes to the chip all in one function
 	set the sleep_enable bit on the device (so wake up) */
-	if (i2c_reg_update_byte(drv_data->i2c, CONFIG_MPU6050_I2C_ADDR,
-				MPU6050_REG_PWR_MGMT1, MPU6050_SLEEP_EN,
+	if (i2c_reg_update_byte(drv_data->i2c, CONFIG_MPU9250_I2C_ADDR,
+				MPU9250_REG_PWR_MGMT1, MPU9250_SLEEP_EN,
 				0) < 0) {
 		SYS_LOG_ERR("Failed to wake up chip.");
 		return -EIO;
@@ -174,7 +180,7 @@ int mpu6050_init(struct device *dev)
 
 	/* set accelerometer full-scale range */
 	for (i = 0; i < 4; i++) {
-		if (BIT(i+1) == CONFIG_MPU6050_ACCEL_FS) {
+		if (BIT(i+1) == CONFIG_MPU9250_ACCEL_FS) {
 			break;
 		}
 	}
@@ -184,9 +190,9 @@ int mpu6050_init(struct device *dev)
 		return -EINVAL;
 	}
 
-	if (i2c_reg_write_byte(drv_data->i2c, CONFIG_MPU6050_I2C_ADDR,
-			       MPU6050_REG_ACCEL_CFG,
-			       i << MPU6050_ACCEL_FS_SHIFT) < 0) {
+	if (i2c_reg_write_byte(drv_data->i2c, CONFIG_MPU9250_I2C_ADDR,
+			       MPU9250_REG_ACCEL_CFG,
+			       i << MPU9250_ACCEL_FS_SHIFT) < 0) {
 		SYS_LOG_ERR("Failed to write accel full-scale range.");
 		return -EIO;
 	}
@@ -195,7 +201,7 @@ int mpu6050_init(struct device *dev)
 
 	/* set gyroscope full-scale range */
 	for (i = 0; i < 4; i++) {
-		if (BIT(i) * 250 == CONFIG_MPU6050_GYRO_FS) {
+		if (BIT(i) * 250 == CONFIG_MPU9250_GYRO_FS) {
 			break;
 		}
 	}
@@ -205,17 +211,17 @@ int mpu6050_init(struct device *dev)
 		return -EINVAL;
 	}
 
-	if (i2c_reg_write_byte(drv_data->i2c, CONFIG_MPU6050_I2C_ADDR,
-			       MPU6050_REG_GYRO_CFG,
-			       i << MPU6050_GYRO_FS_SHIFT) < 0) {
+	if (i2c_reg_write_byte(drv_data->i2c, CONFIG_MPU9250_I2C_ADDR,
+			       MPU9250_REG_GYRO_CFG,
+			       i << MPU9250_GYRO_FS_SHIFT) < 0) {
 		SYS_LOG_ERR("Failed to write gyro full-scale range.");
 		return -EIO;
 	}
 
-	drv_data->gyro_sensitivity_x10 = mpu6050_gyro_sensitivity_x10[i];
+	drv_data->gyro_sensitivity_x10 = mpu9250_gyro_sensitivity_x10[i];
 
-#ifdef CONFIG_MPU6050_TRIGGER //important
-	if (mpu6050_init_interrupt(dev) < 0) {
+#ifdef CONFIG_MPU9250_TRIGGER //important
+	if (mpu9250_init_interrupt(dev) < 0) {
 		SYS_LOG_DBG("Failed to initialize interrupts.");
 		return -EIO;
 	}
@@ -224,8 +230,12 @@ int mpu6050_init(struct device *dev)
 	return 0;
 }
 
-struct mpu6050_data mpu6050_driver;
+struct mpu9250_data mpu9250_driver;
 
-DEVICE_AND_API_INIT(mpu6050, CONFIG_MPU6050_NAME, mpu6050_init, &mpu6050_driver,
+
+// CONFIG_MPU9250_NAME
+// name of driver/chip
+// this is what we want to search for in imu 
+DEVICE_AND_API_INIT(mpu9250, CONFIG_MPU9250_NAME, mpu9250_init, &mpu9250_driver,
 		    NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,
-		    &mpu6050_driver_api);
+		    &mpu9250_driver_api);
